e06ca8e41b0bac98d5d4ddbb0d7abf6131658133
Armen2552 <armenhayrapetyan@gmail.com>
2023-01-05 12:58:54 -0500

    05-01

---
+++ react-app/src/Components/header/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Header extends Component{
+
+
+
+
+    render(){
+
+        return <>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ratione.</p>
+        </>
+    }
+}
+
+export default Header
+
